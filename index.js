12e831c93b3897d24933f8ee6f4579c5ff6a95d9
FabricioAlvarez <fabricio.alvarez@interbrainco.com>
2023-01-25 22:01:37 -0300

    cambios en configuraciones2

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started port ${PORT}`));
