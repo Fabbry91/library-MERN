29eef1d0aa708773c16cfe70f0dc839cba6b59bd
FabricioAlvarez <fabricio.alvarez@interbrainco.com>
2023-01-28 23:39:29 -0300

    ajustes container row

--- client/src/components/pages/home/HomeInformation.js
+++ client/src/components/pages/home/HomeInformation.js
@@ -13,7 +13,7 @@ export const HomeInformation = () => {
                             <div className="info-left flex">
                                 <h4 className="m-0" style={{ fontWeight: 500 }}>Productos Destacados</h4>
                                 <p style={{fontSize:'14px', fontWeight:400, color:'#646464'}}>Resaltadores, lapices y colores</p>
-                                <Link to="#" className="h6" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver más...</Link>
+                                
                             </div>
                             <div className="info-rigth flex-grow">
                                 <img className='img-banner' src={`${process.env.PUBLIC_URL}/assets/img/pens.png`} alt="user" />
@@ -26,7 +26,7 @@ export const HomeInformation = () => {
                             <div className="info-left flex">
                                 <h4 className="m-0" style={{ fontWeight: 500 }}>Nueva Colección</h4>
                                 <p style={{fontSize:'14px', fontWeight:400, color:'#646464'}}>Adhesivos y pegamentos</p>
-                                <Link to="#" className="h6" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver más...</Link>
+                                
                             </div>
                             <div className="info-rigth flex-grow">
                                 <img className='img-banner' src={`${process.env.PUBLIC_URL}/assets/img/glues.png`} alt="user" />
@@ -39,7 +39,7 @@ export const HomeInformation = () => {
                             <div className="info-left flex">
                                 <h4 className="m-0" style={{ fontWeight: 500 }}>Productos Traiding</h4>
                                 <p style={{fontSize:'14px', fontWeight:400, color:'#646464'}}>Pintureria</p>
-                                <Link to="#" className="h6" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver más...</Link>
+                                
                             </div>
                             <div className="info-rigth flex-grow">
                                 <img className='img-banner' src={`${process.env.PUBLIC_URL}/assets/img/paints.png`} alt="user" />
